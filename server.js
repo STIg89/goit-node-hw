75462880d519d8ffba8b408f18b42acd2de2118d
STIg89 <andreystig89@gmail.com>
2023-05-01 15:56:12 +0300

    add

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://STIg:lZUh1z3QOY3t6vH4@cluster0.nvncyjk.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });

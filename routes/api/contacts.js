8ff1e45099b6e60f91c33e40310132c640dd28c8
STIg89 <andreystig89@gmail.com>
2023-05-02 00:23:25 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const controllers = require("../../controllers/contacts");
 const schemas = require("../../schemas/contacts");
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
 const router = express.Router();
 
 router.get("/", controllers.listContacts);
 
-router.get("/:contactId", controllers.getContactById);
-
-router.delete("/:contactId", controllers.removeContact);
+router.get("/:contactId", isValidId, controllers.getContactById);
 
 router.post("/", validateBody(schemas.addSchema), controllers.addContact);
 
 router.put(
   "/:contactId",
+  isValidId,
   validateBody(schemas.updateSchema),
   controllers.updateContact
 );
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  controllers.updateStatusContact
+);
+
+router.delete("/:contactId", isValidId, controllers.removeContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const app = require("./app");
 const mongoose = require("mongoose");
-
-const DB_HOST =
-  "mongodb+srv://STIg:lZUh1z3QOY3t6vH4@cluster0.nvncyjk.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const app = require("./app");
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    console.log("Database connection successful");
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
